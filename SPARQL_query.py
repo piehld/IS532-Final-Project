27fa7f7a02e03f202628d792ddba5b5106114c81
piehld <piehld@gmail.com>
2020-04-21 19:52:49 -0500

    Beginning work with SPARQL queries in Python

---
+++ SPARQL_query.py
@@ -0,0 +1,102 @@
+# Guided Reference: https://janakiev.com/blog/wikidata-mayors/
+
+import requests
+import pandas as pd
+from collections import OrderedDict
+
+"""
+Notes:
+    - war (Q198)
+    - participant (P710)
+    - United States of America (Q30)
+
+    - Example war: Gulf War (Q37643)
+        - instance of : war
+        - start time : 2 August 1990
+        - end time : 28 Februrary 1991
+        - participant : ...
+        - (*No death information*)
+        
+    - Example war: Vietnam War (Q8740)
+        - (same fields as above)
+        - number of deaths : 1,291,425
+                           : 4,211,459
+"""
+
+url = 'https://query.wikidata.org/sparql'
+
+## This query selects all events that are an instance of war
+# query = """
+# SELECT 
+  # ?event ?eventLabel
+# WHERE {
+  # ?event wdt:P31/wdt:P279* wd:Q198.
+  # SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+# }
+# """
+
+## This query selects all events that are an instance of war, AND that the event participant is United States
+query = """
+SELECT 
+  ?event ?eventLabel
+WHERE {
+  ?event wdt:P31/wdt:P279* wd:Q198.
+  ?event wdt:P710 wd:Q30.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+"""
+r = requests.get(url, params = {'format': 'json', 'query': query})
+# print(r.text)
+data = r.json()
+print(data)
+
+wars = []
+for item in data['results']['bindings']:
+    wars.append(OrderedDict({
+        'war': item['event']['value'],
+        'warLabel': item['eventLabel']['value']}))
+
+df = pd.DataFrame(wars)
+print(df)
+
+
+
+
+
+## Example from online guide: https://janakiev.com/blog/wikidata-mayors/
+
+# query = """
+# SELECT 
+  # ?countryLabel ?population ?area ?medianIncome ?age
+# WHERE {
+  # ?country wdt:P463 wd:Q458.
+  # OPTIONAL { ?country wdt:P1082 ?population }
+  # OPTIONAL { ?country wdt:P2046 ?area }
+  # OPTIONAL { ?country wdt:P3529 ?medianIncome }
+  # OPTIONAL { ?country wdt:P571 ?inception. 
+    # BIND(year(now()) - year(?inception) AS ?age)
+  # }
+  # SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+# }
+# """
+# r = requests.get(url, params = {'format': 'json', 'query': query})
+# data = r.json()
+# print(data)
+
+
+# countries = []
+# for item in data['results']['bindings']:
+    # countries.append(OrderedDict({
+        # 'country': item['countryLabel']['value'],
+        # 'population': item['population']['value'],
+        # 'area': item['area']['value'] 
+            # if 'area' in item else None,
+        # 'medianIncome': item['medianIncome']['value'] 
+            # if 'medianIncome' in item else None,
+        # 'age': item['age']['value'] 
+            # if 'age' in item else None}))
+
+# df = pd.DataFrame(countries)
+# df.set_index('country', inplace=True)
+# df = df.astype({'population': float, 'area': float, 'medianIncome': float, 'age': float})
+# print(df.head())
